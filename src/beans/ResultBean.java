5db048d79f9544271bf246f8e92bfd8a48ad42e1
Ignacio Loayza Campos <ignacio.1505@gmail.com>
2017-01-25 01:46:53 -0300

    Se implementa ResultBean y UserDAO

---
+++ src/beans/ResultBean.java
@@ -0,0 +1,145 @@
+package beans;
+
+import java.util.ArrayList;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import dao.UserDAO;
+
+
+
+
+@ManagedBean(name="result")
+@SessionScoped
+public class ResultBean {
+	private String numeroSolicitud;
+	private String numeroRegistro;
+	private String clase;
+	private String denominacion;
+	private String titular_nombre;
+	private String titular_pais;
+	private String representante_nombre;
+	
+	
+	public ArrayList<ResultBean> getRecords(){
+		return UserDAO.getData();
+		
+	}
+	
+	
+	public String getNumeroSolicitud() {
+		return numeroSolicitud;
+	}
+	public void setNumeroSolicitud(String numeroSolicitud) {
+		this.numeroSolicitud = numeroSolicitud;
+	}
+	public String getNumeroRegistro() {
+		return numeroRegistro;
+	}
+	public void setNumeroRegistro(String numeroRegistro) {
+		this.numeroRegistro = numeroRegistro;
+	}
+	public String getClase() {
+		return clase;
+	}
+	public void setClase(String clase) {
+		this.clase = clase;
+	}
+	public String getDenominacion() {
+		return denominacion;
+	}
+	public void setDenominacion(String denominacion) {
+		this.denominacion = denominacion;
+	}
+	public String getTitular_nombre() {
+		return titular_nombre;
+	}
+	public void setTitular_nombre(String titular_nombre) {
+		this.titular_nombre = titular_nombre;
+	}
+	public String getTitular_pais() {
+		return titular_pais;
+	}
+	public void setTitular_pais(String titular_pais) {
+		this.titular_pais = titular_pais;
+	}
+	public String getRepresentante_nombre() {
+		return representante_nombre;
+	}
+	public void setRepresentante_nombre(String representante_nombre) {
+		this.representante_nombre = representante_nombre;
+	}
+	public String getEstadoSolicitud() {
+		return estadoSolicitud;
+	}
+	public void setEstadoSolicitud(String estadoSolicitud) {
+		this.estadoSolicitud = estadoSolicitud;
+	}
+	public String getFechaPresentacion() {
+		return fechaPresentacion;
+	}
+	public void setFechaPresentacion(String fechaPresentacion) {
+		this.fechaPresentacion = fechaPresentacion;
+	}
+	public String getFechaPublicacion() {
+		return fechaPublicacion;
+	}
+	public void setFechaPublicacion(String fechaPublicacion) {
+		this.fechaPublicacion = fechaPublicacion;
+	}
+	public String getTitular_comuna() {
+		return titular_comuna;
+	}
+	public void setTitular_comuna(String titular_comuna) {
+		this.titular_comuna = titular_comuna;
+	}
+	public String getFechaInstancia() {
+		return fechaInstancia;
+	}
+	public void setFechaInstancia(String fechaInstancia) {
+		this.fechaInstancia = fechaInstancia;
+	}
+	public String getFechaVencimiento() {
+		return fechaVencimiento;
+	}
+	public void setFechaVencimiento(String fechaVencimiento) {
+		this.fechaVencimiento = fechaVencimiento;
+	}
+	public String getEstadoInstancia() {
+		return estadoInstancia;
+	}
+	public void setEstadoInstancia(String estadoInstancia) {
+		this.estadoInstancia = estadoInstancia;
+	}
+	public String getObservacionInstancia() {
+		return observacionInstancia;
+	}
+	public void setObservacionInstancia(String observacionInstancia) {
+		this.observacionInstancia = observacionInstancia;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getTipoMarca() {
+		return tipoMarca;
+	}
+	public void setTipoMarca(String tipoMarca) {
+		this.tipoMarca = tipoMarca;
+	}
+	private String estadoSolicitud;
+	private String fechaPresentacion;
+	private String fechaPublicacion;
+	private String titular_comuna;
+	private String fechaInstancia;
+	private String fechaVencimiento;
+	private String estadoInstancia;
+	private String observacionInstancia;
+	private String categoria;
+	private String tipoMarca;
+	
+	
+}
---
+++ src/dao/UserDAO.java
@@ -0,0 +1,78 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import Connection.conexion;
+import beans.ResultBean;
+import beans.SearchBean;
+
+public class UserDAO {
+	
+	private static ResultSet myRs;
+
+	public static ArrayList<ResultBean> getData(){
+		try {
+			//Creamos la conexion a la BD
+			Connection my_conn;
+			my_conn = conexion.crearConexion();
+			//Creamos el objeto Statement, en este se ejecutara la query final
+			Statement myStmt = my_conn.createStatement();
+	
+			//Se envia la sentencia y el objeto myStmt para ser ejecutado en setMyRs()
+			setMyRs(SearchBean.getSQL_stmt(), myStmt);
+			
+			ArrayList<ResultBean> resultados_busqueda = new ArrayList<ResultBean>();
+			
+			boolean found = false;
+			while(myRs.next()){
+				ResultBean r = new ResultBean();
+				
+				r.setTitular_nombre(myRs.getString(1));
+				r.setTitular_pais(myRs.getString(2));
+				r.setTitular_comuna(myRs.getString(9));
+				r.setRepresentante_nombre(myRs.getString(3));
+				r.setNumeroSolicitud(myRs.getString(4));
+				r.setNumeroRegistro(myRs.getString(5));
+				r.setEstadoSolicitud(myRs.getString(6));
+				r.setFechaPresentacion(myRs.getString(7));
+				r.setFechaPublicacion(myRs.getString(8));
+				r.setFechaInstancia(myRs.getString(10));
+				r.setFechaVencimiento(myRs.getString(11));
+				r.setEstadoInstancia(myRs.getString(12));
+				r.setObservacionInstancia(myRs.getString(13));
+				r.setCategoria(myRs.getString(14));
+				r.setTipoMarca(myRs.getString(15));
+				
+				found = true;
+			}
+			//Cerramos la conexion...
+			myRs.close();
+			if (found) {
+                return resultados_busqueda;
+            } else {
+                return null; // no entires found
+            }
+			
+			
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println("\n Error en getData -->" + e.getMessage());
+			e.printStackTrace();
+			return null;
+			
+		}
+
+			
+	}
+
+	public ResultSet getMyRs() {
+		return myRs;
+	}
+
+	public static void setMyRs(String stmt, Statement myStmt) throws SQLException {
+		myRs = myStmt.executeQuery(stmt);
+	}
+}
